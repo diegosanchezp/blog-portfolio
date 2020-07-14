4c7f64ca919674d1d62cabf0332ecad328d2efe1
Diego Sanchez <diegosandmg@gmail.com>
2020-07-14 14:10:34 -0400

    feat: responsive design first approach

--- themes/portfolio-theme/assets/css/tailwind.config.js
+++ themes/portfolio-theme/assets/css/tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
-    colors: {
-      // This colors are defined on the theme stylesheet
-      primary: 'var(--color-primary)',
-      secondary: 'var(--color-secondary)',
-      lightwhite: 'var(--color-lightwhite)'
-    },
     extend: {
+      colors: {
+        // This colors are defined on the theme stylesheet
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        lightwhite: 'var(--color-lightwhite)'
+      },
+      screens: {
+        'sp': {'max-width': '639px'}
+      }
     },
   },
   variants: {},
