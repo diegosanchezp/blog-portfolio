de21222798de196d6245e5523f738721cab68e24
Diego Sanchez <diegosandmg@gmail.com>
2020-08-02 13:59:01 -0400

    feat: envelope dialog polyfill
    fix: firefox envelope huge icon

--- themes/portfolio-theme/assets/js/dialog.js
+++ themes/portfolio-theme/assets/js/dialog.js
@@ -3,6 +3,24 @@ const dialog = document.querySelector("#email-dialog");
 const envelope = document.querySelector("#envelope");
 const closeDialog = dialog.querySelector("#close-email-dialog");
 const contactMe = document.querySelector("#contact-me-home");
+// If dialog not supported
+if (typeof dialog.showModal !== "function") {
+  // Load polyfill, warning bloadted +25kB
+  const head = document.querySelector("head");
+  // Load dialog css
+  const polyfillCSS = document.createElement('link');
+  polyfillCSS.setAttribute('rel', 'stylesheet');
+  polyfillCSS.setAttribute('href', '/css/dialog-polyfill.css');
+  head.appendChild(polyfillCSS);
+  // Load dialog script 
+  const polyfill = document.createElement('script');
+  polyfill.setAttribute('src', '/js/dialog-polyfill.js');
+  head.appendChild(polyfill);
+
+  polyfill.onload = () => {
+    dialogPolyfill.registerDialog(dialog);
+  }
+}
 
 envelope.addEventListener("click", ()=>{
   dialog.showModal();
